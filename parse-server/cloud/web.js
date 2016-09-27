1ba631b8bbfc1b8f4e9069b5372e26aa00907829
ChungHyup <tractar@naver.com>
2016-09-27 21:54:11 +0900

    implement scrapHtml. Now it can save ItemList and Item.

--- parse-server/cloud/web.js
+++ parse-server/cloud/web.js
@@ -1,43 +1,66 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var Parse = require('parse');
+var request_ = require('request');
+var cheerio = require('cheerio'); //import
 
-var scrapHtml = function(){
+var scrapHtml = function(request, response){
 
   var url = "http://www.mutnam.com/product/list.html?cate_no=264";
   var Item = Parse.Object.extend("Item");
+  var ItemList = Parse.Object.extend("ItemList");
 
-  request(url, function(error, response, body) {
+  request_(url, function(error, resp, body) {
     if (error) throw error;
 
     var $ = cheerio.load(body);
 
-  var postElements = $("ul.prdList.grid4 li.xans-record-");
-    postElements.each(function() {
-      var item = new Item();
-      var itemPrice;
-      var promotion = $(this).find("div.description div.icon div.promotion>img").prop('src');
-      var itemName = $(this).find("div.description strong a span:nth-child(2)").text();
-      var imgSrc = $(this).find("div.thumbnail img").attr('src');
+    var itemList = new ItemList();
 
-
-      if(itemName !== undefined && itemName !== ''){
-        item.set("name", itemName);
-        if(promotion !== undefined){
-          itemPrice = $(this).find("div.description ul li:nth-child(3)>span").text();
-        }else{
-          itemPrice = $(this).find("div.description ul li:nth-child(2)>span").text();
-        }
-        item.set("price", itemPrice);
-        item.set("imgsrc", imgSrc);
-        item.set("url", "http://www.mutnam.com/");
-
-        var tempText = item.get('name')+'\n'+item.get('price')+'\n'+item.get('url')+'\n'+item.get('imgsrc')+'\n';
-        console.log(tempText);
+    itemList.save(null, {
+      success: function(itemList) {
+        console.log('success to save itemList');
+        _itemSave();
+      },
+      error: function(itemList, error) {
+        console.log('Failed to create new object, with error code: ' + error.message);
       }
     });
+
+    function _itemSave() {
+      var promises = [];
+
+      var postElements = $("ul.prdList.grid4 li.xans-record-");
+      postElements.each(function() {
+        var item = new Item();
+        var itemPrice;
+        var promotion = $(this).find("div.description div.icon div.promotion>img").prop('src');
+        var itemName = $(this).find("div.description strong a span:nth-child(2)").text();
+        var imgSrc = $(this).find("div.thumbnail img").attr('src');
+
+        if(itemName !== undefined && itemName !== ''){
+          item.set("name", itemName); //item.setName(itemName);
+          if(promotion !== undefined){
+            itemPrice = $(this).find("div.description ul li:nth-child(3)>span").text();
+          }else{
+            itemPrice = $(this).find("div.description ul li:nth-child(2)>span").text();
+          }
+          item.set("price", itemPrice);
+          item.set("imgsrc", imgSrc);
+          item.set("url", "http://www.mutnam.com/");
+          item.set("ItemListId", itemList.id);
+          item.set("ItemList", itemList);
+
+          promises.push(item.save());
+        }
+      });
+
+      Parse.Promise.when(promises).then(function() {
+        console.log("success!!");
+        response.success();
+      }, function (error) {
+        console.log("error! in promise");
+        response.error();
+      });
+    }
   });
 
 };
-
-scrapHtml();
+module.exports.scrapHtml = scrapHtml;
