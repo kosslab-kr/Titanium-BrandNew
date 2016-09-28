4a00de585c8029d37438b0f21c6b9aa1c70c21d5
dbtmddus <dbtmddus112@naver.com>
2016-09-28 20:46:18 +0900

    no message

--- parse-server/cloud/dtd.js
+++ parse-server/cloud/dtd.js
@@ -4,33 +4,37 @@ var Parse = require('parse');
 
 var scrapHtml = function(){
 
-  var url = "http://www.coupang.com/np/categories/130800?eventCategory=categoryLeftMenu&eventLabel=";
+  var url = "http://www.chicfox.co.kr/shop/shopbrand.html?xcode=020&type=P";
   var Item = Parse.Object.extend("Item");
 
   request(url, function(error, response, body) {
     if (error) throw error;
-
+    //console.log(body);
     var $ = cheerio.load(body);
 
-  var postElements = $("ul.prdList.grid4 li.xans-record-");
+  var postElements = $("div.item_list.list");
     postElements.each(function() {
       var item = new Item();
-      var itemPrice;
-      var promotion = $(this).find("div.description div.icon div.promotion>img").prop('src');
-      var itemName = $(this).find("div.description strong a span:nth-child(2)").text();
-      var imgSrc = $(this).find("div.thumbnail img").attr('src');
 
+      var itemName = $(this).find("li.pname font font").text();
+      var promotion = $(this).find("li.icons>img").prop('src');
+      var itemPrice;
+      var imgSrc = $(this).find("li.thumb img").attr('src');
+      //item.set("url", "http://www.chicfox.co.kr/index.html");
+      //var link = $(this).find(li.thumb a).text();
+      //link = link+url;
+      //("div.thumbnail img").attr('src');
 
       if(itemName !== undefined && itemName !== ''){
         item.set("name", itemName);
         if(promotion !== undefined){
-          itemPrice = $(this).find("div.description ul li:nth-child(3)>span").text();
+          itemPrice = $(this).find("div.item_list.list span.price-strike").text();
         }else{
-          itemPrice = $(this).find("div.description ul li:nth-child(2)>span").text();
+          itemPrice = $(this).find("div.item_list.list span.price").text();
         }
         item.set("price", itemPrice);
         item.set("imgsrc", imgSrc);
-        item.set("url", "http://www.mutnam.com/");
+        item.set("url", "http://www.chicfox.co.kr/index.html");
 
         var tempText = item.get('name')+'\n'+item.get('price')+'\n'+item.get('url')+'\n'+item.get('imgsrc')+'\n';
         console.log(tempText);
